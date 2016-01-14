461b085bb7cc2db8d518d486ad8b8b8e35a6b0f7
Jason Haley <jason@jasonhaley.com>
2016-01-14 13:30:32 -0500

    Initial add

---
+++ AzureKeyVault.Configuration/AzureKeyVaultConfigurationProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.CodeDom;
+using Microsoft.Extensions.Configuration;
+
+namespace AzureKeyVault.Configuration
+{
+    public class AzureKeyVaultConfigurationProvider : ConfigurationProvider
+    {
+        public AzureKeyVaultConfigurationProvider() : this(string.Empty)
+        { }
+
+        public AzureKeyVaultConfigurationProvider(string prefix)
+        {
+            // TODO:
+        }
+    }
+}
---
+++ AzureKeyVault.Configuration/AzureKeyVaultExtensions.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Microsoft.Extensions.Configuration;
+
+namespace AzureKeyVault.Configuration
+{
+    public static class AzureKeyVaultExtensions
+    {
+        public static IConfigurationBuilder AddAzureKeyVault(this IConfigurationBuilder builder)
+        {
+            builder.Add(new AzureKeyVaultConfigurationProvider());
+            return builder;
+        }
+
+        public static IConfigurationBuilder AddAzureKeyVault(this IConfigurationBuilder builder, string prefix)
+        {
+            builder.Add(new AzureKeyVaultConfigurationProvider(prefix));
+            return builder;
+        }
+    }
+}
