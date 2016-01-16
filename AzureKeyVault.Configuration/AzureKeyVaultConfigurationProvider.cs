7fba2717068592802670b0b3a6b70361fb083a49
Jason Haley <jason@jasonhaley.com>
2016-01-15 19:58:41 -0500

    Initial implementation

--- AzureKeyVault.Configuration/AzureKeyVaultConfigurationProvider.cs
+++ AzureKeyVault.Configuration/AzureKeyVaultConfigurationProvider.cs
@@ -1,16 +1,73 @@
-﻿using System.CodeDom;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.KeyVault;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 
 namespace AzureKeyVault.Configuration
 {
     public class AzureKeyVaultConfigurationProvider : ConfigurationProvider
     {
-        public AzureKeyVaultConfigurationProvider() : this(string.Empty)
-        { }
-
-        public AzureKeyVaultConfigurationProvider(string prefix)
+        public AzureKeyVaultConfigurationProvider(string connectionString, IEnumerable<string> secretNames)
         {
-            // TODO:
+            if (connectionString == null)
+            {
+                throw new ArgumentNullException(nameof(connectionString));
+            }
+
+            if (secretNames == null)
+            {
+                throw new ArgumentNullException(nameof(secretNames));
+            }
+
+            ConnectionInfo = new AzureKeyVaultConnectionInfo(connectionString);
+            SecretsNames = secretNames;
+        }
+
+
+        internal AzureKeyVaultConnectionInfo ConnectionInfo { get; private set; }
+        protected IEnumerable<string> SecretsNames { get; private set; }
+
+        public override void Load()
+        {
+            var data = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+            if (SecretsNames.Any())
+            {
+                var keyVaultClient = GetKeyVaultClient();
+                foreach (var secretName in SecretsNames)
+                {
+                    var secret =
+                        keyVaultClient.GetSecretAsync(ConnectionInfo.GetKeyVaultUrl(), secretName)
+                            .GetAwaiter()
+                            .GetResult();
+                    data[secretName] = secret.Value;
+                }
+            }
+
+            Data = data;
+        }
+
+        protected internal KeyVaultClient GetKeyVaultClient()
+        {
+            return new KeyVaultClient(GetAccessToken, GetHttpClient());
+        }
+
+        private async Task<string> GetAccessToken(string authority, string resource, string scope)
+        {
+            var clientCredential = new ClientCredential(ConnectionInfo.ClientId, ConnectionInfo.ClientSecret);
+            var context = new AuthenticationContext(authority, TokenCache.DefaultShared);
+            var result = await context.AcquireTokenAsync(resource, clientCredential);
+
+            return result.AccessToken;
+        }
+
+        private static HttpClient GetHttpClient()
+        {
+            return new HttpClient(new HttpClientHandler());
         }
     }
 }
--- AzureKeyVault.Configuration/AzureKeyVaultExtensions.cs
+++ AzureKeyVault.Configuration/AzureKeyVaultExtensions.cs
@@ -1,21 +1,15 @@
-﻿
-
+﻿using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 
 namespace AzureKeyVault.Configuration
 {
     public static class AzureKeyVaultExtensions
     {
-        public static IConfigurationBuilder AddAzureKeyVault(this IConfigurationBuilder builder)
+        public static IConfigurationBuilder AddAzureKeyVault(this IConfigurationBuilder builder, string connectionString, IEnumerable<string> secretNames)
         {
-            builder.Add(new AzureKeyVaultConfigurationProvider());
+            builder.Add(new AzureKeyVaultConfigurationProvider(connectionString, secretNames));
             return builder;
         }
-
-        public static IConfigurationBuilder AddAzureKeyVault(this IConfigurationBuilder builder, string prefix)
-        {
-            builder.Add(new AzureKeyVaultConfigurationProvider(prefix));
-            return builder;
-        }
+        
     }
 }
